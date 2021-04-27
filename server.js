ec763b50fa0e8197ab1f0e5a20655f024958058d
Joshua Rully <joshrully@gmail.com>
2021-04-27 10:04:50 -0700

    setup transactions routes and controllers

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const colors = require('colors');
 
 dotenv.config({ path: './config/config.env' });
 
+const transactions = require('./routes/transactions');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hellow'));
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
