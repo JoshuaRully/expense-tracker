1d3d49cfa2b3ce8ac78ae9b6c4e71e68631d0446
Joshua Rully <joshrully@gmail.com>
2021-04-28 19:28:20 -0700

    fix typo in reducer and implement useEffect in TransactionList

--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import React, { useContext, useEffect } from 'react';
 import { Transaction } from './Transaction';
 
-export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+import { GlobalContext } from '../context/GlobalState';
 
+export const TransactionList = () => {
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
